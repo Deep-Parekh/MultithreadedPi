2e18f8b20c699844d4cf13286f8a4a724b982803
Deep <dparekh20@outlook.com>
2019-10-24 23:12:09 -0700

    Need to make research lock.acquire and release to avoid deadlocks

--- multithreaded_pi.py
+++ multithreaded_pi.py
@@ -14,25 +14,10 @@ totalTime = 0
 pi = 0
 start = 0
 lock = threading.Lock()
-startTime = time.time()
+random.seed(time.time())
+threads = []
 
-for i in range(0,numThreads):
-    if i == (numThreads-1):
-        t = threading.Thread(target=calculatePoints, args=(start,POINTS))
-    else:
-        end = start + POINTS/numThreads
-        t = threading.Thread(target=calculatePoints, args=(start,end))
-        start = end
-    t.start()
-
-pi = float(totalCirclePoints)/POINTS
-totalTime = startTime - time.time()
-
-print("It took " + totalTime + " seconds to estimate the value of pi")
-print("The estimate is " + pi)
-
-def calculatePoints(start, end):
-    circlePoints = 0
+def calculate_points(start, end):
     for i in range(start, end):
         x = random.uniform(0,1) 
         y = random.uniform(0,1)
@@ -41,5 +26,26 @@ def calculatePoints(start, end):
 
         if (r <= 1):
             lock.acquire()
+            global totalCirclePoints
             totalCirclePoints += 1
             lock.release()
+
+startTime = time.time()
+
+for i in range(0,numThreads):
+    if i == (numThreads-1):
+        t = threading.Thread(target=calculate_points, args=(start,POINTS,))
+    else:
+        end = int(start + POINTS/numThreads)
+        t = threading.Thread(target=calculate_points, args=(start,end,))
+        threads.append(t)
+        start = end
+    t.start()
+
+for i in range(0,len(threads)):
+    threads[i].join()
+pi = 4.0 * (float(totalCirclePoints)/POINTS)
+totalTime = time.time() - startTime
+
+print('It took %f seconds to estimate the value of pi' %totalTime)
+print("The estimate is %f" %pi)
